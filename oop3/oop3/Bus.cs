49c0f124aa09ab4527b235ff87bdb520f9a6d0e9
Ilya Kazakou <pouutewqer@gmail.com>
2020-10-01 14:00:25 +0300

    Added 3 lab

---
+++ oop3/oop3/Bus.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using Microsoft.VisualBasic;
+
+namespace oop3
+{
+    public class Bus
+    { 
+        //static
+        static readonly DateTime dateCreationOfPark;
+
+        private static ushort quantity = 0;
+
+        //public
+        private const string typeOfVehicle = "Bus";
+
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        private string routeNum;
+
+        public string RouteNum
+        {
+            get
+            {
+                return routeNum;
+            }
+            set
+            {
+                routeNum = value;
+            }
+        }
+
+        private string vehicleMark;
+
+        public string VehicleMark
+        {
+            get
+            {
+                return vehicleMark;
+            }
+            set
+            {
+                vehicleMark = value;
+            }
+        }
+
+        private uint busNum;
+
+        public uint BusNum
+        {
+            get
+            {
+                return busNum;
+            }
+            set
+            {
+                if (value > busNum)
+                    Console.WriteLine("Incorrect value!");
+                else
+                busNum = value;
+            }
+        }
+
+        //private
+        private readonly double ID;
+
+        private uint mileage;
+
+        public uint Mileage
+        {
+            get
+            {
+                return mileage;
+            }
+            set
+            {
+                if(mileage<0)
+                    Console.WriteLine("Incorrect value!");
+                else
+                    mileage = value;
+            }
+        }
+
+
+
+        private int age;
+
+        public int Age
+        {
+            get
+            {
+                return (DateTime.Now.Year - startInUsing.Year);
+            }
+        }
+
+        private DateTime startInUsing;
+
+        public DateTime StartInUsing
+        {
+            get
+            {
+                return startInUsing;
+            }
+        }
+        private void countHash(uint mileage, DateTime startInUsing, string name, out double ID)
+        {
+            ID = (Math.Pow(mileage, startInUsing.Year) / name.Length);
+        }
+
+        public void changeName(ref string fullname)
+        {
+            fullname = Console.ReadLine();
+        }
+
+
+
+        public Bus()
+        {
+            this.name = "Trainee";
+            this.routeNum = "Autodrome";
+            this.vehicleMark = "MAZ";
+            this.busNum = 5683;
+            this.mileage = 0;
+            this.startInUsing = new DateTime(2010,10,3);
+            countHash(mileage, startInUsing, name, out ID);
+            quantity++;
+        }
+
+        public Bus(string fullname = "Trainee"):this()
+        {
+            this.name = fullname;
+        }
+        public Bus(
+           DateTime inUsingYear,
+           string name,
+           string routeNum,
+           string vehicleMark ,
+           uint busNum,
+           uint mileage):this()
+        {
+            if (DateTime.Now.Year - inUsingYear.Year > 100)
+            {
+                Console.WriteLine("Incorrect value!");
+                this.startInUsing = DateTime.Now;
+            }
+            else
+                this.startInUsing = inUsingYear;
+
+            this.name = name;
+            this.routeNum = routeNum;
+            this.vehicleMark = vehicleMark;
+            this.busNum = busNum;
+            this.mileage = mileage;
+        }
+
+        static Bus()
+        {
+            dateCreationOfPark = DateTime.Now;
+            Console.WriteLine("You will see this message only one time.\n Park was created!");
+        }
+
+        public override bool Equals(Object obj)
+        {
+            Bus forOverride = obj as Bus;
+            return (
+                name == forOverride.name &&
+                routeNum == forOverride.routeNum &&
+                vehicleMark == forOverride.vehicleMark &&
+                busNum == forOverride.busNum &&
+                mileage == forOverride.mileage &&
+                startInUsing == forOverride.startInUsing
+                );
+        }
+
+        public override int GetHashCode()
+        {
+            return (int)ID;
+        }
+
+        public override string ToString()
+        {
+            return $"Bus {vehicleMark} number {busNum}, route {routeNum}, meliage {mileage}\n" +
+                $", used since {startInUsing} by {name}";
+        }
+        
+        public void ShowAge()
+        {
+            Console.WriteLine($"Age of bus:{this.Age} year");
+        }
+    }
+    public partial class Bus
+    {
+        public void showClassInfo()
+        {
+            Console.WriteLine($"Quantity of objects:{quantity}");
+        }
+    }
+
+    
+
+}
---
+++ oop3/oop3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace oop3
+{
+
+    class Timetable
+    { 
+        private Timetable(){}
+        public static DateTime firstBus = DateTime.Today;
+    }
+  
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Bus A1 = new Bus(new DateTime(2018, 12, 1),"Franchesko", "11", "VN12", 2009, 13444);
+            Bus A2 = new Bus(new DateTime(2019, 2, 1),"Mario", "11", "VN12", 2000, 18912);
+            Bus B18 = new Bus(new DateTime(2019, 3, 1),"Franciua", "19A", "VN12", 1999, 23900);
+            Bus C1 = new Bus("Abele");
+            Bus trainee = new Bus();
+
+            A1.showClassInfo();
+            //output with using get
+            Console.WriteLine($"Get: {A1.Name},{A2.Mileage},{B18.RouteNum},{C1.VehicleMark},{trainee.StartInUsing}");
+            
+            //set
+            trainee.Name = "Alex";
+            trainee.Mileage = 0;
+            trainee.RouteNum = "S1";
+            trainee.BusNum = 1000;
+
+            A1.Equals(A2);
+            int hash = B18.GetHashCode();
+            Console.WriteLine($"Get type:\n" +
+                $"A1:{A1.GetType()}\n" +
+                $"A2:{A2.GetType()}\n" +
+                $"C1:{C1.GetType()}\n");
+
+
+            Bus[] park = new Bus[]
+                {
+                    A1,
+                    A2,
+                    B18,
+                    C1,
+                    trainee
+                };
+
+            //TASKS
+
+            //1
+            string userCheckRoute = Console.ReadLine();
+
+            foreach(Bus item in park)
+            {
+                if(string.Compare(userCheckRoute, item.RouteNum) == 0)
+                {
+                    Console.WriteLine($"{item.RouteNum}: {item.BusNum}, driver: {item.Name}, {item.Mileage} km");
+                }
+            }
+
+            //2
+            int userInputAge = Convert.ToInt32(Console.ReadLine());
+            foreach(Bus item in park)
+            {
+                if(item.Age > userInputAge)
+                {
+                    Console.WriteLine($"{item.RouteNum}: {item.BusNum}, driver: {item.Name}, {item.Mileage} km");
+                }
+            }
+
+                
+
+            var D12 = new { InUsingDate = new DateTime(2020, 12, 2),
+            Name = "Abella", Route = "19A", VehicleMark = "BELAZ12", BusNum = 1223, Mileage = 512};
+
+            var D13 = new Bus();
+
+            Console.WriteLine($"{D13.Mileage},{D13.BusNum},{D13.RouteNum},{D13.Name}");
+        }
+    }
+}
