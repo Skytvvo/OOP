73167b20ba9d82acf617a82793c88efd6ecbfb6c
Ilya Kazakou <pouutewqer@gmail.com>
2020-11-17 21:58:56 +0300

    Added 10th lab. Completed 1st task

--- oop_10/oop_10/Program.cs
+++ oop_10/oop_10/Program.cs
@@ -10,6 +10,36 @@ namespace oop_10
     {
         static void Main(string[] args)
         {
+            try
+            {
+                InternerResource<string> BY = new InternerResource<string>(4);
+                BY.Add("Onliner");
+                BY.Add("Kufar");
+                BY.Add("TUT.by");
+                BY.Add("4PDA");
+
+                foreach (string item in BY)
+                {
+                    Console.WriteLine(item);
+                }
+
+                //===============
+                Console.Write("\n========================\n\n");
+                //===============
+                BY.RemoveAt(3);
+                BY.Insert(1, "VITALIK");
+
+                foreach (string item in BY)
+                {
+                    Console.WriteLine(item);
+                }
+
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
