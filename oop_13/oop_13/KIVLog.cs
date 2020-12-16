853bbc87a657a2ecc7286856e196331e3892c54e
Ilya Kazakou <pouutewqer@gmail.com>
2020-12-16 16:41:11 +0300

    Added 12th lab: finished 1 task

---
+++ oop_13/oop_13/KIVLog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_13
+{
+    
+    static internal class KIVLog
+    {
+        static public void ListenUserActivity(MethodInfo Method, FileInfo File, DateTime Time, string Path )
+        {
+            StreamWriter OutFile = new StreamWriter(Path, true);
+
+            OutFile.WriteLine($"[{Time.Date}]: User use {Method.Name} from {File.FullName};");
+
+            OutFile.Close();
+        }
+        static public void ListenUserActivity(MethodInfo method, FileInfo File, DateTime Time)
+        {
+            ListenUserActivity(method, File, Time, Directory.GetCurrentDirectory() + @"\UserActivity.log");
+        }
+
+        static public void ListenUserActivity(MethodInfo method, FileInfo File)
+        {
+            ListenUserActivity(method, File, DateTime.Now);
+        }
+    }
+}
---
+++ oop_13/oop_13/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_13
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Directory.GetCurrentDirectory());
+        
+        }
+    }
+}
