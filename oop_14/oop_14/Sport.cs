e6a040f20c55d0c8a7a0c7d101e234eb1673c845
Ilya Kazakou <pouutewqer@gmail.com>
2020-12-17 15:17:37 +0300

    Added 14th lab

---
+++ oop_14/oop_14/Sport.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_14
+{
+    [Serializable]
+    public class Sport : Inventory
+    {
+        [NonSerialized]
+        public string noSeriazable = "nope";
+
+        public string yesSeriazable = "oh,my";
+        public override string ToString()
+        {
+
+            return ($"{this.GetType()}, values: {this.name}");
+        }
+        public Sport(string Name) : base(Name) { }
+        public Sport() : base("Me") { }
+    }
+}
