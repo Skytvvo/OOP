5c136cd97db7b8fcd0bb5964f206e3450e956507
Ilya Kazakou <pouutewqer@gmail.com>
2020-12-24 05:46:31 +0300

    Added 16th lab

---
+++ oop_15/oop_15/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace oop_15
+{
+    class Program
+    {
+        static string path;
+        static int n = 20;
+        static int g = 1;
+        static void Main(string[] args)
+        {
+            path = @".\Lab.log";
+            using (StreamWriter clearFile = new StreamWriter(path, false))
+            {
+                clearFile.WriteLine("");
+            }
+
+            Process[] currentProcesses = Process.GetProcesses();
+            foreach(var item in currentProcesses)
+            {
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter(path, true))
+                    {
+                       
+                            sw.WriteLine("=======================================");
+                        sw.WriteLine($"ID:{item.Id}");
+                        sw.WriteLine($"Name:{item.ProcessName}");
+                        sw.WriteLine($"Priority:{item.BasePriority}");
+                        sw.WriteLine($"Time-start:{item.StartTime.ToString()}");
+                        sw.WriteLine($"State:{item.Responding.ToString()}");
+                        sw.WriteLine($"Runt-itme:{item.GetLifetimeService()}");
+                        sw.WriteLine("=======================================");
+                    }
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine($"Domain name:{Thread.GetDomain().FriendlyName}");
+                sw.WriteLine($"Configuration details: {Thread.GetDomain().SetupInformation}");
+                sw.WriteLine($"Assemlies: \n");
+                foreach(var item in Thread.GetDomain().GetAssemblies())
+                {
+                    sw.WriteLine($"Name: {item.GetName()}");
+                }
+            }
+            AppDomain newDomain = AppDomain.CreateDomain("NewDomain");
+            
+            newDomain.Load(Assembly.GetExecutingAssembly().GetName());
+            AppDomain.Unload(newDomain);
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Oper param = new Oper(userInput, path);
+            Thread newThread = new Thread(new ThreadStart(param.countO));
+            newThread.Name = "My thread";
+
+            newThread.Start();
+            newThread.Join();
+
+            Thread evenTh = new Thread(new ThreadStart( evenum));
+            Thread oddTh = new Thread(new ThreadStart(oddnum));
+
+            evenTh.Start();
+            oddTh.Start();
+            
+            evenTh.Priority = ThreadPriority.Highest;
+            oddTh.Priority = ThreadPriority.Lowest;
+
+
+
+            evenTh.Join();
+            
+            oddTh.Join();
+           
+            TimerCallback tiktak = new TimerCallback(clockTIK);
+            Timer tm = new Timer(tiktak, 60, 0, 1000);
+            Console.ReadLine();
+        }
+
+        static public void evenum()
+        {
+            for(int i = 0; i < n; i++)
+            {
+                if(i %2==0)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(260);
+                }
+            }
+        }
+
+        static public void oddnum()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(460);
+                }
+            }
+        }
+
+        static public void clockTIK(object par)
+        {
+            if (g % 2 == 0)
+                Console.WriteLine($"Tak - {g}");
+            else
+                Console.WriteLine($"Tik - {g}");
+            g++;
+        }
+
+    }
+    public class Oper
+    {
+        int x;
+        Thread savedThread = Thread.CurrentThread;
+        string path;
+        public void countO()
+        {
+            using (StreamWriter sw = new StreamWriter(this.path, true))
+            {
+                try
+                {
+                    for (int i = 1; i < x; i++)
+                    {
+                        
+                        Console.WriteLine($"Thread status: {Thread.CurrentThread.ThreadState}");
+                        sw.WriteLine($"Thread status: {Thread.CurrentThread.ThreadState}");
+
+                        Console.WriteLine($"Thread name: {Thread.CurrentThread.Name}");
+                        sw.WriteLine($"Thread name: {Thread.CurrentThread.Name}");
+
+                        Console.WriteLine($"Thread priority: {Thread.CurrentThread.Priority}");
+                        sw.WriteLine($"Thread priority: {Thread.CurrentThread.Priority}");
+
+                        Console.WriteLine($"Thread id: {Thread.CurrentThread.GetHashCode()}");
+                        sw.WriteLine($"Thread id: {Thread.CurrentThread.GetHashCode()}");
+                        Thread.Sleep(500);
+                        
+                    }
+                }
+                catch(Exception e)
+                {
+               
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+            }
+
+        }
+
+        public Oper(int x, string path)
+        {
+            this.x = x;
+            this.path = path;
+        }
+
+    }
+    
+}
---
+++ oop_16/oop_16/Program.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace oop_16
+{
+
+    
+    class Program
+    {
+      
+
+        static void Main(string[] args)
+        {
+
+            Stopwatch checker = Stopwatch.StartNew();
+            Task<uint> resheto = Task.Factory.StartNew<uint>(() => {
+                var numbers = new List<uint>();
+                int n = 100000;
+                for (var i = 2u; i < n; i++)
+                {
+                    numbers.Add(i);
+                }
+                for (var i = 0; i < numbers.Count; i++)
+                {
+                    for (var j = 2u; j < n; j++)
+                    {
+                        numbers.Remove(numbers[i] * j);
+                    }
+                }
+
+                return (uint)numbers.Count;
+            });
+            Console.Write($"ID of task: {resheto.Id} ");
+            if (resheto.IsCompleted)
+                Console.WriteLine($"was completed, status: {resheto.Status}");
+            else
+                Console.WriteLine($"was interrupted, status: {resheto.Status}");
+
+            checker.Stop();
+            Console.WriteLine($"First task: {checker.Elapsed.TotalMilliseconds} ms");
+            checker.Reset();
+
+            //second task
+            CancellationTokenSource tokenSource =new CancellationTokenSource();
+            checker.Start();
+            Task<uint> reshetoTask2 = Task.Factory.StartNew<uint>(() => {
+                var numbers = new List<uint>();
+                int n = 100000;
+                for (var i = 2u; i < n; i++)
+                {
+                    numbers.Add(i);
+                }
+                
+                for (var i = 0; i < numbers.Count; i++)
+                {
+                    for (var j = 2u; j < n; j++)
+                    {
+                        numbers.Remove(numbers[i] * j);
+                    }
+                }
+
+                return (uint)numbers.Count;
+            }, tokenSource.Token);
+            tokenSource.Cancel();
+
+            Console.Write($"ID of task: {reshetoTask2.Id} was canceled");
+           
+            
+
+            checker.Stop();
+            Console.WriteLine($"Second task: {checker.Elapsed.TotalMilliseconds} ms");
+
+
+            Task<int> varX = Task.Factory.StartNew(() =>
+            {
+                return DateTime.Now.Second;
+            });
+
+            varX.Wait();
+
+            Task<int> varY = Task.Factory.StartNew(() =>
+            {
+                return DateTime.Now.Millisecond;
+            });
+
+            varY.Wait();
+
+            Task<int> varZ = Task.Factory.StartNew(() =>
+            {
+                return DateTime.Now.Month;
+            });
+
+            varZ.Wait();
+
+            Task countFor = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine($"Result: {varZ.Result + varX.Result * varY.Result}");
+            });
+            countFor.ContinueWith((taskVAR) =>
+            {
+                Console.WriteLine("Continue with...");
+            }).Wait();
+            
+             var  awaiterCountFor = countFor.GetAwaiter();
+             awaiterCountFor.OnCompleted(() =>{
+                awaiterCountFor.GetResult();
+                Console.WriteLine("Awaiter...");
+            });
+            Thread.Sleep(100);
+
+
+            var nums = new List<int>();
+            int qua = 100000;
+
+            for (var i = 0; i < qua; i++)
+            {
+                nums.Add(i);
+            }
+            Thread.Sleep(500);
+            checker.Restart();
+            Parallel.For(1, qua, Factorial);
+            checker.Stop();
+            Console.WriteLine($"For completed in {checker.Elapsed.TotalMilliseconds} ms");
+
+            Console.WriteLine();
+            checker.Restart();
+            var forEachResult = Parallel.ForEach<int>(nums, Factorial);
+            checker.Stop();
+            Console.WriteLine($"ForEach completed in {checker.ElapsedMilliseconds} ms");
+
+            checker.Restart();
+            for (var i = 0; i <qua;i++)
+            {
+                Factorial(i);
+            }
+            checker.Stop();
+
+            Console.WriteLine($"Standart for: {checker.ElapsedMilliseconds} ms");
+
+            Parallel.Invoke(()=> {
+
+                int result = 1;
+
+                for (int i = 1; i <= qua; i++)
+                {
+                    result *= i;
+                }
+            });
+
+            BlockingCollection<string> blockRoll = new BlockingCollection<string>();
+            Task Seller = Task.Factory.StartNew(() =>
+            {
+                List<string> items = new List<string>() { "Axe", "Pickaxe", "Torch", "Torch", "Carrot", "Golden carrot" };
+                Random ransd = new Random();
+                int store = 0;
+                for(int i = 0;items.Count!=0; i++)
+                {
+                    store = ransd.Next(0, items.Count - 1);
+                    Console.WriteLine($"Item {items[store]} was delivered");
+                    blockRoll.Add(items[store]);
+                    items.RemoveAt(store);
+                    Thread.Sleep(store);
+                }
+                blockRoll.CompleteAdding();
+            });
+
+            Task steve = Task.Factory.StartNew(() =>
+            {
+                string ff;
+                while(!blockRoll.IsCompleted)
+                {
+                    if (blockRoll.TryTake(out ff))
+                        Console.WriteLine($"Selled {ff}");
+                    else
+                        Console.WriteLine("Store empty");
+
+                }
+            });
+
+            Seller.Wait();
+            steve.Wait();
+
+            asyncFactorial();
+
+        }
+
+        async static void asyncFactorial(int x = 100000)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Running async ...");
+            await Task.Run(() => Factorial(x));
+            Console.WriteLine("Ready!");
+        }
+
+        static void Factorial(int x = 100000)
+        {
+            int result = 1;
+
+            for (int i = 1; i <= x; i++)
+            {
+                result *= i;
+            }
+        }
+    }
+}
